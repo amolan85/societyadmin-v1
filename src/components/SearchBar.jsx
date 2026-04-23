44b9537d9681fcc5b7ec8a1e073a557d40f09e49
amolan85 <amolan85@gmail.com>
2026-04-24 02:55:56 +0530

    updated

--- src/components/SearchBar.jsx
+++
@@ -1,8 +0,0 @@
-// src/components/SearchBar.jsx
-export default function SearchBar() {
-  return (
-    <div className="card" style={{ marginTop: 20 }}>
-      🔍 Start searching here
-    </div>
-  );
-}
--- src/components/StatCard.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/StatCard.jsx
-export default function StatCard({ title, value }) {
-  return (
-    <div className="card stat-card">
-      <p>{title}</p>
-      <h2>{value}</h2>
-    </div>
-  );
-}
