4f2d05c5770fe13994588be1368b3a072b8cbdfb
amolan85 <amolan85@gmail.com>
2026-07-08 18:09:48 +0530

    Code

--- src/utils/Url.jsx
+++ src/utils/Url.jsx
@@ -1,4 +1,4 @@
-const UrlData = "https://societymanagementapi.onrender.com/api/"
+//const UrlData = "https://societymanagementapi.onrender.com/api/"
 //const UrlData = "http://129.154.244.122/api/"
 
  
@@ -8,13 +8,14 @@ const UrlData = "https://societymanagementapi.onrender.com/api/"
   
 //const UrlData = "http://192.168.0.197:5001/api/"
  
-//const UrlData = "http://192.168.0.124:5001/api/"
+const UrlData = "http://192.168.0.124:5001/api/"
  
 
 export default UrlData;
 
 
 /**
+ * 
  * Noticeboard API need to do pagination
  * Events API need to do pagination
  * get Member by id
