f4ebcd98bc37c0d6fcd00965a8430561a27e8435
amolan85 <amolan85@gmail.com>
2026-07-08 14:44:39 +0530

    Accounts update

--- src/utils/Url.jsx
+++ src/utils/Url.jsx
@@ -6,9 +6,13 @@
  
 // const UrlData = "http://192.168.0.172:5001/api/"
  
-const UrlData = "http://192.168.0.197:5001/api/"
+const UrlData = "http://192.168.0.124:5001/api/"
 
 
 export default UrlData;
 
 
+/**
+ * Noticeboard API need to do pagination
+ * Events API need to do pagination
+ */
