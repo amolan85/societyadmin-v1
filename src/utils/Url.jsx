4ebe2d009d3cb734f55825e2da437950ee0ca2aa
amolan85 <amolan85@gmail.com>
2026-07-10 11:43:19 +0530

    Polles Updates

--- src/utils/Url.jsx
+++ src/utils/Url.jsx
@@ -1,4 +1,4 @@
-const UrlData = "https://societymanagementapi.onrender.com/api/"
+//const UrlData = "https://societymanagementapi.onrender.com/api/"
  
 //const UrlData = "http://129.154.244.122/api/"
  
@@ -12,7 +12,7 @@ const UrlData = "https://societymanagementapi.onrender.com/api/"
 //const UrlData = "http://192.168.0.197:5001/api/"
  
 
-//const UrlData = "http://192.168.1.23:5001/api/"
+const UrlData = "http://192.168.0.124:5001/api/"
  
 
 export default UrlData;
